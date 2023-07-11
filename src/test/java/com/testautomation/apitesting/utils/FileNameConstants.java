beedf0148730407aeea9121e36d8294d5eb6b45d
Your Name <you@example.com>
2023-07-11 16:10:25 +0530

    Added new class

--- src/test/java/com/testautomation/apitesting/utils/FileNameConstants.java
+++ src/test/java/com/testautomation/apitesting/utils/FileNameConstants.java
@@ -9,5 +9,9 @@ public class FileNameConstants {
 	public static final String POST_API_REQUEST_BODY = BASE_PATH+"postapirequestbody.json";
 	
 	public static final String JSON_SCHEMA = BASE_PATH+ "expectedJSONSchema.txt";
+	
+	public static final String TOKEN_REQUEST_BODY = BASE_PATH + "tokenapirequestbody.txt";
+	
+	public static final String PUT_API_REQUEST_BODY = BASE_PATH + "putapirequestbody.txt";
 
 }
